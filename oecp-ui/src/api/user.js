143526542c268b9f3d1e6db018b278f20e7f5646
liy <liy@sinmn.com>
2020-03-12 10:33:36 +0800

    oecp 初始化

---
+++ oecp-ui/src/api/user.js
@@ -0,0 +1,24 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/oecp-ui/user/login',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/oecp-ui/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/oecp-ui/user/logout',
+    method: 'post'
+  })
+}
---
+++ oecp-ui/src/views/dashboard/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="dashboard-container">
+    <!-- <div class="dashboard-text">name: {{ name }}</div> -->
+    <div class="dashboard-text">一个错误码平台   One ErrorCode Platform</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+
+.dashboard-text{
+   font:  normal 20px/30px '微软雅黑', 'Arial';
+   color: #33CC52;
+   text-align: left;
+}
+</style>
